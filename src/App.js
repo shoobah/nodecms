cb85aac6b2678316b88e1fd4826c6f80d2421213
Anders Sjöberg <anders.sjoberg@visma.com>
2016-09-18 02:19:40 +0200

    Mer stuff

--- src/App.js
+++ src/App.js
@@ -8,10 +8,10 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Welcome to NodeCMS</h2>
         </div>
         <p className="App-intro">
-          To get started, edit kåda <code>src/App.js</code> and save to reload.
+          To  get started, edit kåda <code>src/App.js</code> and save to reload.
         </p>
       </div>
     );
